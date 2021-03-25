9d74f12b228993666e4d628a70fba31323e1fb22
Hassan Adekoya <hadekoya@gmail.com>
2021-03-25 00:06:22 -0400

    cleanup

--- src/components/HomepageComponent.js
+++ src/components/HomepageComponent.js
@@ -13,14 +13,6 @@ class Homepage extends Component {
       <div className="jumbotron jumbotron-fluid"><div className="container"><div className="row row-content"><div className="col-md-7"><div id="homeCarousel" className="carousel slide" data-ride="carousel"><ol className="carousel-indicators"><li data-target="#homeCarousel" data-slide-to="0" className="active"></li><li data-target="#homeCarousel" data-slide-to="1" className=""></li><li data-target="#homeCarousel" data-slide-to="2" className=""></li></ol><div class="carousel-inner"><div class="carousel-item active"><img className="d-block w-100 rounded" src="/assets/images/logan-item.png" alt="Breadcrumb Trail Campground"/></div><div className="carousel-item"><img className="d-block w-100 rounded" src="/assets/images/kelly-item.png" alt="React Lake Campground"/></div><div className="carousel-item"><img className="d-block w-100 rounded" src="/assets/images/deva-item.png" alt="Chrome River Campground"/></div></div><a className="carousel-control-prev" href="#homeCarousel" role="button" data-slide="prev"><span className="carousel-control-prev-icon"></span> <span className="sr-only">Previous</span> </a><a className="carousel-control-next" href="#homeCarousel" role="button" data-slide="next"><span className="carousel-control-next-icon"></span> <span className="sr-only">Next</span></a></div></div><div className="col-md-3 mx-auto"><h2>Empyrean Cakes</h2><p>Heavenly delight at your doorstep! Let us bring our exquisite treats to your event leaving yours guest with great memories. There should always be a cake for every occassion.</p><p className="lead"><a className="btn btn-primary btn-lg" href="/home" role="button">Learn more</a></p></div></div></div></div>
       <div className="container-fluid p-3">
         <div className="row">
-          <div className="col-11 border border-warning rounded p-3 mx-auto">
-            <span>
-              <b>Note</b>: COVID restriction in place. Please make appointment
-              before visiting store
-            </span>
-          </div>
-        </div>
-        <div className="row">
           <div className="col-md-1"></div>
           <div className="col-md-3">
             <div className="card">
@@ -76,6 +68,14 @@ class Homepage extends Component {
           <div className="col-md-1"></div>
         </div>
       </div>
+      <div className="row mb-2">
+          <div className="col-11 border border-warning rounded p-3 mx-auto">
+            <span>
+              <b>Note</b>: COVID restriction in place. Please make appointment
+              before visiting store
+            </span>
+          </div>
+        </div>
       <RenderCampaign campaign={this.props.feature} title={this.props.title}/>
        </React.Fragment>
     );
